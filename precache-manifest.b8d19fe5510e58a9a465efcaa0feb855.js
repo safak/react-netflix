7df727ffbc15327b27176e85248a142fac317327
safak <safakkocaoglu@hotmail.com>
2020-06-05 16:30:48 +0200

    Updates

---
+++ precache-manifest.b8d19fe5510e58a9a465efcaa0feb855.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afbedf816f49adc1436994df51b95263",
+    "url": "/react-netflix/index.html"
+  },
+  {
+    "revision": "b2bec105c436ad66b1e9",
+    "url": "/react-netflix/static/css/2.7f383e30.chunk.css"
+  },
+  {
+    "revision": "31c31acaa0ee8e0147fd",
+    "url": "/react-netflix/static/css/main.9e4f6d3b.chunk.css"
+  },
+  {
+    "revision": "b2bec105c436ad66b1e9",
+    "url": "/react-netflix/static/js/2.f74c0456.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-netflix/static/js/2.f74c0456.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31c31acaa0ee8e0147fd",
+    "url": "/react-netflix/static/js/main.d6efa306.chunk.js"
+  },
+  {
+    "revision": "b91a6b7305491c76dd55",
+    "url": "/react-netflix/static/js/runtime-main.426a0ea8.js"
+  },
+  {
+    "revision": "f279ee4f33ef6cda7ada086af6fa2462",
+    "url": "/react-netflix/static/media/1.f279ee4f.jpg"
+  },
+  {
+    "revision": "04afa858e71fbfcc7108a12e4ad3494c",
+    "url": "/react-netflix/static/media/2.04afa858.jpg"
+  },
+  {
+    "revision": "255737e108f7b513a44162bcd007076e",
+    "url": "/react-netflix/static/media/forrest.255737e1.webp"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/react-netflix/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/react-netflix/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/react-netflix/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/react-netflix/static/media/slick.f97e3bbf.svg"
+  }
+]);
