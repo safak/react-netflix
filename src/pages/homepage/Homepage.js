d912ca5637e1407e6ede64fd793801671b1b9480
safak <safakkocaoglu@hotmail.com>
2020-06-05 21:56:10 +0200

    dependency has been fixed

--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
 import "./homepage.scss";
-import Card from "../../components/card/Card";
-import Slider from "react-slick";
 import axios from "axios";
 import Detail from "../../components/details/Details";
 import Navbar from "../../components/navbar/Navbar";
